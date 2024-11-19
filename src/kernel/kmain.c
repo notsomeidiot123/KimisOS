faa833ee22052d83bdfca6ddc34367120d07d46b
notsomeidiot123 <kiminosei321@gmail.com>
2024-11-19 12:02:28 -0500

    Edit kmain for testing

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -7,16 +7,32 @@
 #include "drivers/cpuio.h"
 #include "system/scheduler.h"
 
+void test(){
+    printf("Hello from thread!\n");
+    // thread_exit(1);
+}
+void test1(){
+    printf("Hello from thread1!\n");
+    uint32_t exit_code = -1;
+    uint32_t pid = thread_start(test);
+    // for(;;);
+    printf("Start");
+    // printf("PID: %d", pid);
+    for(;;);
+}
 extern void kmain(kernel_info_t *kernel_info){
     init_serial();
     pm_init(kernel_info);
     init_idt();
     init_pic(32);
     pic_disable();
-    init_scheduler();
     pic_setmask(0xfe, PIC1_DATA);
+    //TODO: Map and load filesystem and disk modules
+    //TODO: Start Initial Process
+    uint32_t exit_code = -1;
+    init_scheduler();
+    thread_start(test1);
     enable_interrupts();
-    
     for(;;);
     return;
 }
